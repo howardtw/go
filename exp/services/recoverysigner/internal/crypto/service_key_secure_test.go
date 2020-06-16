cb1ec081afbd5a1994d2e39e137fed67e4233051
Howard Chen <howard@stellar.org>
2020-06-16 15:07:17 +0800

    add service key secure test and mock

---
+++ exp/services/recoverysigner/internal/crypto/service_key_secure_test.go
@@ -0,0 +1,42 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewSecureServiceKey(t *testing.T) {
+	ksPriv := generateHybridEncryptionPrivateKeyKeyset(t)
+	encryptedKsPriv, err := mockAEAD{}.Encrypt(ksPriv, nil)
+	require.NoError(t, err)
+
+	serviceKey, err := newSecureServiceKey(mockKMSClient{}, "aws-kms://key-uri", encryptedKsPriv)
+	require.NoError(t, err)
+	assert.NotNil(t, serviceKey)
+	assert.NotNil(t, serviceKey.remote)
+	assert.NotNil(t, serviceKey.keyset)
+	assert.NotNil(t, serviceKey.hybridEncrypt)
+}
+
+func TestSecureServiceKey_encryptDecrypt(t *testing.T) {
+	ksPriv := generateHybridEncryptionPrivateKeyKeyset(t)
+	encryptedKsPriv, err := mockAEAD{}.Encrypt(ksPriv, nil)
+	require.NoError(t, err)
+
+	serviceKey, err := newSecureServiceKey(mockKMSClient{}, "aws-kms://key-uri", encryptedKsPriv)
+
+	plaintext := []byte("secure message")
+	contextInfo := []byte("context info")
+	ciphertext, err := serviceKey.Encrypt(plaintext, contextInfo)
+	require.NoError(t, err)
+
+	plaintext2, err := serviceKey.Decrypt(ciphertext, contextInfo)
+	require.NoError(t, err)
+	assert.Equal(t, plaintext, plaintext2)
+
+	// context info not matching will result in a failed decryption
+	_, err = serviceKey.Decrypt(ciphertext, []byte("wrong info"))
+	assert.Error(t, err)
+}
