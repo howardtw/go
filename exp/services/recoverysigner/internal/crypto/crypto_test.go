c4f83ab701e566f248d875a5e3202cd7165a9a1c
Howard Chen <howard@stellar.org>
2020-06-18 12:09:39 +0800

    add test for new encrypter and decrypter

---
+++ exp/services/recoverysigner/internal/crypto/crypto_test.go
@@ -0,0 +1,22 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewEncrypter(t *testing.T) {
+	ksPriv := generateHybridEncryptionPrivateKeyKeyset(t)
+	encrypter, err := NewEncrypter("", string(ksPriv))
+	require.NoError(t, err)
+	assert.IsType(t, (*InsecureServiceKey)(nil), encrypter)
+}
+
+func TestNewDecrypter(t *testing.T) {
+	ksPriv := generateHybridEncryptionPrivateKeyKeyset(t)
+	decrypter, err := NewDecrypter("", string(ksPriv))
+	require.NoError(t, err)
+	assert.IsType(t, (*InsecureServiceKey)(nil), decrypter)
+}
